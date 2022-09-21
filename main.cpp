ec8d1ac4101472f7b8f759aafa576082a54bd406
NurulFikrySolakhudin23 <113243611+NurulFikrySolakhudin23@users.noreply.github.com>
2022-09-21 22:39:21 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
-#include <iostream>
+#include <iostream>8
+#define harga 4500.02
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+int main () {
+  float jumlah, total;
+  cout << "Masukan jumlah barang = ";
+  cin >> jumlah; total = harga*jumlah;
+  cout << "Yang harus di bayar = " <<total<<endl;
+
+  return 0;
 }
